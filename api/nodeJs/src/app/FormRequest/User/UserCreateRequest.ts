8eef90c65c6e45405ca0d9ed38c23ddf78288fdb
powderpower <ace7upp@gmail.com>
2021-05-17 15:43:39 +0300

    Перенес запросы форм

--- api/nodeJs/src/app/FormRequest/User/UserCreateRequest.ts
+++ api/nodeJs/src/app/FormRequest/User/UserCreateRequest.ts
@@ -1,10 +1,10 @@
-import ValidationRequest from '../base/ValidationRequest';
-import { MESSAGE_ALPHA_NUM, MESSAGE_MIN_LENGTH, MESSAGE_EMAIL, MESSAGE_PASSWORD_CONFIRM, MESSAGE_EMAIL_EXISTS } from '../base/Messages';
-import { USER_NAME_MIN_LENGTH, USER_NAME_MAX_LENGTH, USER_PASSWORD_MIN_LENGTH, USER_PASSWORD_MAX_LENGTH } from '../base/ValidationConstants';
+import ValidationRequest from '../../../Framework/FormRequest/Base/ValidationRequest';
+import { MESSAGE_ALPHA_NUM, MESSAGE_MIN_LENGTH, MESSAGE_EMAIL, MESSAGE_PASSWORD_CONFIRM, MESSAGE_EMAIL_EXISTS } from '../../../Framework/FormRequest/Base/Messages';
+import { USER_NAME_MIN_LENGTH, USER_NAME_MAX_LENGTH, USER_PASSWORD_MIN_LENGTH, USER_PASSWORD_MAX_LENGTH } from '../../../Framework/FormRequest/Base/ValidationConstants';
 import { IsEmail, IsAlphanumeric, IsLength } from "validator.ts/decorator/Validation";
 import { ToString } from "validator.ts/decorator/Sanitization";
-import UserRepository from '../../app/Repository/UserRepository';
-import User from '../../app/Entity/User';
+import UserRepository from '../../Repository/UserRepository';
+import User from '../../Entity/User';
 
 export default class UserCreateRequest extends ValidationRequest
 {
