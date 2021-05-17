b7eba9803404dcac824294f73f618d39901ac6fc
powderpower <ace7upp@gmail.com>
2021-05-17 15:36:32 +0300

    перенес команду

---
+++ api/nodeJs/src/app/Repository/RoleRepository.ts
@@ -0,0 +1,63 @@
+import Role from '../Entity/Role';
+
+export default class RoleRepository
+{
+    /**
+     * Поиск по ид.
+     * 
+     * @param  number id
+     * @return Promise<Role|null>
+     */
+    public static async findById(id: number): Promise<Role|null>
+    {
+        const role = await Role.findOne({ where: { id } });
+
+        if (role instanceof Role) {
+            return role;
+        }
+
+        return null;
+    }
+
+    /**
+     * Поиск по имени.
+     * 
+     * @param  string name
+     * @return Promise<Role|null>
+     */
+    public static async findByName(name: string): Promise<Role|null>
+    {
+        const role = await Role.findOne({where: { name }});
+
+        if (role instanceof Role) {
+            return role;
+        }
+
+        return null;
+    }
+
+    /**
+     * Создание новой роли.
+     * 
+     * @param  string name
+     * @param  string description
+     * @return Promise<Role>
+     */
+    public static async createNew(name: string, description: string): Promise<Role>
+    {
+        const role = new Role;
+
+        role.name = name;
+        role.description = description;
+
+        await role.save();
+
+        return role;
+    }
+
+    public static async createPermission(role: Role)
+    {
+        
+    }
+
+}
