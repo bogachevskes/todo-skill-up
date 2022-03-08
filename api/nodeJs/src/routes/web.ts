76de7688a5db535fb068d8b37911b8b0fdfc27e0
Andrey Bogachevskiy <ace7upp@gmail.com>
2022-03-08 13:40:04 +0300

    Убрал артефакты

--- api/nodeJs/src/routes/web.ts
+++ api/nodeJs/src/routes/web.ts
@@ -1,17 +1,7 @@
 import RoutesCollection from '../Framework/Http/Router/RoutesCollection';
 import Route from '../Framework/Http/Router/Route';
 
-import SiteController from '../app/Http/Controllers/SiteController';
 import AuthController from '../app/Http/Controllers/AuthController';
-
-RoutesCollection.add(
-    new Route(
-        'GET',
-        '/',
-        SiteController,
-        'actionIndex'
-    ),
-);
 
 RoutesCollection.add(
     new Route(
