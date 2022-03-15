b17495f82115a24a26d33f3d8332612054154c12
Andrey Bogachevskiy <ace7upp@gmail.com>
2022-03-15 19:42:48 +0300

    Добавил точку входа приложения websocket

--- websocket/nodeJs/src/app.js
+++ websocket/nodeJs/src/app.js
@@ -10,6 +10,24 @@ const io = new Server(server);
 io.on('connection', (socket) => {
     console.log(`User Connected: ${socket.id}`);
 
+    socket.on('send_message', (data) => {
+        console.log(`User sent a message:`, data);
+
+        socket.emit('receive_message', 'hi client');
+    });
+
+    socket.on('join_room', (data) => {
+        
+        console.log(`User ${socket.id} joined a room`);
+
+    });
+
+    setInterval(function() {
+        socket.emit('receive_message', 'are you okey?');
+
+        console.log('sent check to client');
+    }, 3000);
+
     socket.on('disconnect', () => {
         console.log(`User Disconnected: ${socket.id}`);
     })
