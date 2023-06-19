74e28c05625775850d0194dd444c6662ce3f950c
Andrey Bogachevskiy <ace7upp@gmail.com>
2023-06-20 00:14:19 +0300

    изменил докер-конфигурацию. добавил мультистейжд сборку

--- api/nodeJs/ormconfig.js
+++ api/nodeJs/ormconfig.js
@@ -7,7 +7,7 @@ module.exports = {
     "port": process.env.DB_PORT,
     "username": process.env.DB_USER,
     "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
+    "database": process.env.DB_NAME,
     "logging": process.env.DB_LOGGING,
     "synchronize": process.env.DB_SYNC,
     "entities": [
