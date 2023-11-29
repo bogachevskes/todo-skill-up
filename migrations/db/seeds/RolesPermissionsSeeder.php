2d9302588865a72d8a2692944eeecdbac4d45510
Andrey Bogachevskiy <ace7upp@gmail.com>
2023-11-29 13:43:49 +0300

    удалил более неиспользуемое разрешение

--- migrations/db/seeds/RolesPermissionsSeeder.php
+++ migrations/db/seeds/RolesPermissionsSeeder.php
@@ -19,14 +19,9 @@ class RolesPermissionsSeeder extends AbstractSeed
             [
                 'parent' => 'administrator',
                 'child' => '/admin/users',
-            ],
-            [
-                'parent' => 'administrator',
-                'child' => '/admin/users/todo',
             ]
         ];
 
-        // Вставляем данные в таблицу auth_item_child
         $table = $this->table('auth_item_child');
         $table->insert($data)->save();
     }
