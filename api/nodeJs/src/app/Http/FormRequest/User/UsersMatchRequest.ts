71a121a1a12115a8c525fd9c8dba3aa8226aad4b
Andrey Bogachevskiy <ace7upp@gmail.com>
2023-12-05 17:13:12 +0300

    исправил баг выборки всех пользователей при передачи строки из множественных пробелов

--- api/nodeJs/src/app/Http/FormRequest/User/UsersMatchRequest.ts
+++ api/nodeJs/src/app/Http/FormRequest/User/UsersMatchRequest.ts
@@ -1,19 +1,38 @@
 import ValidationRequest from '../../../../Framework/FormRequest/Base/ValidationRequest';
-import { IsLength } from "validator.ts/decorator/Validation";
-import { ToString } from "validator.ts/decorator/Sanitization";
+import {IsLength} from "validator.ts/decorator/Validation";
+import {ToString, Trim} from "validator.ts/decorator/Sanitization";
 
 export default class UsersMatchRequest extends ValidationRequest
 {
-    @IsLength(5, 100, { message: 'Введите минимум 5 символов' })
     @ToString()
+    @Trim()
+    @IsLength(5, 100, { message: 'Введите минимум 5 символов поля Email' })
     public email: string;
 
-    @IsLength(5, 100, { message: 'Введите минимум 5 символов' })
     @ToString()
+    @Trim()
+    @IsLength(5, 100, { message: 'Введите минимум 5 символов поля Имя' })
     public name: string;
+
+    public skipMissingProperties: boolean = true;
 
     /**
      * @see ValidationRequest
      */
-    public skipMissingProperties: boolean = true;
+    protected getCustomValidations(): Function[]
+    {
+        return [
+            async () => {
+                if (this.email !== undefined && this.email.length !== 0) {
+                    return;
+                }
+
+                if (this.name !== undefined && this.name.length !== 0) {
+                    return;
+                }
+
+                return this.validateManual(false, 'Не заданы параметры поиска', 'query_params_undefined');
+            },
+        ];
+    }
 }
