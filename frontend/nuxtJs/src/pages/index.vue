6a0ecc389b291912bd7395efb807d413e25491c2
Andrey Bogachevskiy <ace7upp@gmail.com>
2023-12-05 18:24:02 +0300

    изменен способ переключения между режимами авторизации/регистрации на домашней странице

--- frontend/nuxtJs/src/pages/index.vue
+++ frontend/nuxtJs/src/pages/index.vue
@@ -1,33 +1,40 @@
 <template>
     <section class="hero is-info is-fullheight" style="min-height: 600px">
         <div class="hero-body">
-            <div class="container">
+            <div v-if="isLogged === false" class="container">
                 <div class="columns is-vcentered">
-                    <Invite />
-                    <Register v-if="isLogged === false" />
+                    <div
+                        class="column is-6-tablet is-7-desktop is-8-widescreen"
+                    >
+                        <TheProjectTitle />
+                    </div>
+                    <Login v-if="onRegistration === false" />
+                    <Register v-if="onRegistration === true" />
                 </div>
             </div>
+            <Boards v-if="isLogged === true" />
         </div>
     </section>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
-import Invite from '@/components/home/Invite';
+import TheProjectTitle from '@/components/TheProjectTitle';
 import Register from '@/components/home/Register';
+import Login from '@/components/home/Login';
+import Boards from '@/components/user-boards/Boards';
 
 export default {
     name: 'IndexPage',
     components: {
-        Invite,
+        TheProjectTitle,
+        Login,
         Register,
-    },
-    beforeRouteLeave (to, from, next) {
-        this.$eventBus.hideInviteIntro();
-        next();
+        Boards,
     },
     computed: {
-        ...mapGetters('todo', ['isLogged']),
+        ...mapGetters('user', ['isLogged']),
+        ...mapGetters('invite', ['onRegistration']),
     },
 };
 </script>
