2ba449f0e865658f214e71c1f5e78abdeb33180d
Andrey Bogachevskiy <ace7upp@gmail.com>
2023-12-05 18:15:04 +0300

    переименована сущность пользователя

--- frontend/nuxtJs/src/plugins/models/User.ts
+++ frontend/nuxtJs/src/plugins/models/User.ts
@@ -1,12 +1,12 @@
 import CommandContext from '../base/CommandContext';
 
-export default class UserIdentity extends CommandContext {
+export default class User extends CommandContext
+{
     constructor() {
         super();
         this.setMultiple({
             token: null,
             userId: null,
-            groups: [],
             permissions: [],
         });
     }
