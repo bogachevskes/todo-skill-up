4ca1f9b1a3e3b4c9d73c3ff1cd530c7226fc27a8
Andrey Bogachevskiy <ace7upp@gmail.com>
2024-02-14 19:29:53 +0300

    Объявления конструкций управления состоянием базы в тесте доступности списка разрешений доски для владельца доски заменены на трейты

--- tests/tests/integration/api/BoardPermissionsCest.php
+++ tests/tests/integration/api/BoardPermissionsCest.php
@@ -2,49 +2,16 @@
 
 namespace tests\integration\api;
 
-use Tests\Support\ApiTester;
+use Tests\Support\{
+    ApiTester,
+    UserTrait,
+    HashTrait,
+    UserBoardTrait,
+};
 
 class BoardPermissionsCest
 {
-    private function createUser(ApiTester $I, int $nameId): array
-    {
-        $user = [
-            'name' => "boardStatusesCestGuest$nameId",
-            'email' => "board.statuses.cest.guest$nameId@todo-list.com",
-            'password' => base64_encode('secret'),
-        ];
-
-        $I->haveInDatabase('users', $user);
-
-        $userId = $I->grabFromDatabase('users', 'id', ['email' => $user['email']]);
-
-        return [
-            $userId,
-            $user['email'],
-        ];
-    }
-
-    private function createBoard(ApiTester $I, int $nameId, int $userId): int
-    {
-        $board = [
-            'name' => "user board cest name $nameId",
-            'description' => "user board cest description $nameId",
-        ];
-
-        $I->haveInDatabase('boards', $board);
-
-        $boardId = $I->grabFromDatabase('boards', 'id', ['name' => $board['name']]);
-
-        $boardUser = [
-            'board_id' => $boardId,
-            'user_id' => $userId,
-            'role_id' => 1,
-        ];
-
-        $I->haveInDatabase('boards_users', $boardUser);
-
-        return $boardId;
-    }
+    use UserTrait, HashTrait, UserBoardTrait;
 
     public function testBoardPermission(ApiTester $I): void
     {
@@ -63,10 +30,12 @@ class BoardPermissionsCest
             ]);
         }
 
-        [$userId, $email] = $this->createUser($I, 1);
-        $boardId = $this->createBoard($I, 1, $userId);
+        [$boardOwnerUserId, $boardOwnerUserEmail] = $this->createUser($I);
+        $boardId = $this->createBoard($I, $boardOwnerUserId);
 
-        $I->haveHttpHeader('X-BASE-AUTH', $email);
+        $this->assignUserToBoardAsOwner($I, $boardId, $boardOwnerUserId);
+
+        $I->haveHttpHeader('X-BASE-AUTH', $boardOwnerUserEmail);
 
         $I->sendGet("/v1/boards/$boardId/permissions");
         $I->seeResponseCodeIs(200);
@@ -77,10 +46,12 @@ class BoardPermissionsCest
     {
         $I->wantTo('Отсутствие в ответе разрешений доски, прочих разрешений, являющихся системными');
 
-        [$userId, $email] = $this->createUser($I, 2);
-        $boardId = $this->createBoard($I, 2, $userId);
+        [$boardOwnerUserId, $boardOwnerUserEmail] = $this->createUser($I);
+        $boardId = $this->createBoard($I, $boardOwnerUserId);
 
-        $I->haveHttpHeader('X-BASE-AUTH', $email);
+        $this->assignUserToBoardAsOwner($I, $boardId, $boardOwnerUserId);
+
+        $I->haveHttpHeader('X-BASE-AUTH', $boardOwnerUserEmail);
 
         $notBoardPermission = [
             [
